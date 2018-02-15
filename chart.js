b0f3d2ef396d18d4e40fceb05fecb24634a51e44
Marblez <MatthewWang2020@u.northwestern.edu>
2018-02-14 22:16:10 -0600

    Graph Switch

--- chart.js
+++ chart.js
@@ -1,19 +1,19 @@
 google.charts.load('current', {packages: ['corechart', 'line']});
 google.charts.setOnLoadCallback(drawLineColors);
 
-function drawLineColors() {
+function daily() {
       var data = new google.visualization.DataTable();
       data.addColumn('number', 'X');
       data.addColumn('number', 'Time');
       data.addRows([
         [0, 0],    [1, 10],   [2, 23],  [3, 17],   [4, 18],  [5, 9],
-        [6, 11],   [7, 27],  [8, 33],  [9, 40],  [10, 32], [11, 35],[12, 12], [13, 35], [14, 19], [15, 25], [16, 21], [17, 35],[18, 44],[19, 3],[20, 5],[21, 55],[22, 30],[23, 35],[24, 70],[25, 15],[26, 50],[27, 70],[28, 40],[29, 12],[30, 35],
+        [6, 11],   [7, 27],  [8, 33],  [9, 40],  [10, 32], [11, 35],[12, 12], [13, 35], [14, 19], [15, 25], [16, 21], [17, 35],[18, 44],[19, 3],[20, 5],[21, 55],[22, 30],[23, 35],[24, 70]
 
       ]);
 
       var options = {
         hAxis: {
-          title: 'March 2018'
+          title: 'March 1st - Hourly'
         },
         vAxis: {
           title: 'Minutes Spent'
@@ -24,3 +24,51 @@ function drawLineColors() {
       var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
       chart.draw(data, options);
     }
+
+    function weekly() {
+          var data = new google.visualization.DataTable();
+          data.addColumn('number', 'X');
+          data.addColumn('number', 'Time');
+          data.addRows([
+            [0, 0],    [1, 10],   [2, 23],  [3, 17],   [4, 18],  [5, 9],
+            [6, 11],   [7, 27]
+
+          ]);
+
+          var options = {
+            hAxis: {
+              title: '1st Week of March - Daily'
+            },
+            vAxis: {
+              title: 'Minutes Spent'
+            },
+            colors: ['#a52714', '#097132']
+          };
+
+          var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+          chart.draw(data, options);
+        }
+
+        function monthly() {
+              var data = new google.visualization.DataTable();
+              data.addColumn('number', 'X');
+              data.addColumn('number', 'Time');
+              data.addRows([
+                [0, 0],    [1, 100],   [2, 23],  [3, 17],   [4, 18],  [5, 9],
+                [6, 11],   [7, 27],  [8, 100],  [9, 40],  [10, 32], [11, 35],[12, 12], [13, 35], [14, 19], [15, 25], [16, 21], [17, 35],[18, 44],[19, 3],[20, 5],[21, 55],[22, 30],[23, 35],[24, 70],[25, 15],[26, 50],[27, 70],[28, 40],[29, 12],[30, 35],
+
+              ]);
+
+              var options = {
+                hAxis: {
+                  title: 'March 2018'
+                },
+                vAxis: {
+                  title: 'Minutes Spent'
+                },
+                colors: ['#a52714', '#097132']
+              };
+
+              var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+              chart.draw(data, options);
+            }
